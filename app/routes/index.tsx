4c93db7e1420eb6056361051ef22f5f5ab9e7801
Johnny Chai <hello@johnnychai.com>
2025-08-24 22:48:54 +0800

    fix: trim trailing slash

--- app/routes/index.tsx
+++
@@ -1,7 +0,0 @@
-// app/routes/index.tsx
-
-import { createRoute } from "honox/factory";
-
-// Redirect root ("/") to BASE_URL so that the deployed app on Cloudflare Workers
-// always starts from our intended base path (e.g. "/the-zoo-of-lies")
-export default createRoute((c) => c.redirect("/the-zoo-of-lies/", 301));
--- app/server.ts
+++ app/server.ts
@@ -1,7 +1,12 @@
 import { showRoutes } from "hono/dev";
+import { trimTrailingSlash } from "hono/trailing-slash";
 import { createApp } from "honox/server";
 
+// server entry file
 const app = createApp();
+
+// middleware to handle trailing slash redirects
+app.use(trimTrailingSlash());
 
 showRoutes(app);
 
